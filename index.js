c0a7edd2feb26f7884dc35f331f7aa791eae2b58
horpemee <arifoopeyemi@gmail.com>
2025-06-17 16:53:13 +0100

    changed url

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ const swaggerSpec = swaggerJsdoc({
       description:
         'Express wrapper around the Starlink Enterprise Activation API – docs generated from JSDoc.'
     },
-    servers: [{ url: 'http://localhost:3000' }, { url: "https://starlink-api-project.onrender.com/" }]
+    // servers: [{ url: 'http://localhost:3000' }, { url: "https://starlink-api-project.onrender.com/" }]
+    servers: [{ url: 'http://localhost:3000' }, { url: "https://api.unconnected.support/" }]
+
   },
   // Scan this file for JSDoc @swagger blocks
   apis: [path.join(__dirname, 'index.js')]
