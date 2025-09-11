749f973f93fe5e0da5a0d484b28d22ab9f4adfef
horpemee <arifoopeyemi@gmail.com>
2025-09-11 13:04:43 +0100

    removed an unwanted account

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ app.delete('/api/accounts/:account/user-terminals/:deviceId', async (req, res) =
 app.get('/api/accounts', async (req, res) => {
   try {
     const data = await makeAuthedGet(`/v1/accounts?limit=50&page=0`);
-    const unwantedAccounts = ['ACC-3196223-39704-14', 'ACC-2959688-22725-30', 'ACC-8653096-80387-28',   'ACC-2963072-59271-18', 'ACC-2866843-91611-20', 'ACC-7393314-12390-10', 'ACC-7580055-64428-19'
+    const unwantedAccounts = ['ACC-3196223-39704-14', 'ACC-2959688-22725-30', 'ACC-8653096-80387-28',   'ACC-2963072-59271-18', 'ACC-2866843-91611-20', 'ACC-7393314-12390-10', 'ACC-DF-8944908-16857-17', 'ACC-7580055-64428-19'
 
 ];
     // Filter out unwanted accounts
@@ -293,7 +293,7 @@ app.get('/api/accounts', async (req, res) => {
       'ACC-6814367-50278-22': 'PCSWiFi4Every1',
       'ACC-7071161-50554-7': 'WirelessLink',
       'ACC-DF-8910267-22774-3': 'Comnet',
-      'ACC-DF-8944908-16857-17': 'Cominicalo',
+      'ACC-DF-8944908-16857-17': 'CMC Service Network',
       'ACC-DF-8914998-17079-20': 'KakumaVentures',
       // 'ACC-7580055-64428-19': 'Unconnected Partner 3',
       'ACC-7393314-12390-10': 'TESTER API ACCOUNT'
